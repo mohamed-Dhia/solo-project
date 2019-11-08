d21271f2dd2bb8e530c01c51fb07ab0765e709ef
mohameddhialol <dhiaweslati16@gmail.com>
2019-11-08 13:00:56 +0100

    dsdqsd

--- main.js
+++ main.js
@@ -35,8 +35,30 @@ function setTheScene() {
 	//$('body').css({'background-image':'url(' + events[0].backGroundPic + ')','background-size':'100% 100%','background-repeat':'no-repeat','height' : '800px'})
 	// $('body').css('background-image','url(https://i.pinimg.com/originals/49/df/93/49df93c9996e7c80736724c2cea3b897.jpg)')
 	$('body').css('background-image','url(' + events[0].backGroundPic + ')');
-	$('.prompt').append('<div class =  event id = ' + 0 + '>'+ events[0].bfscript + '</div>').css('background-repeat',)
-	$('.prompt').append('<button> next>> </button>')
+	$('.prompt').append('<div class =  event id = ' + 0 + '>'+ events[0].bfscript + '</div>')
+	$('.prompt').append('<button class = advance id = actionScene> next>> </button>')
+	$('#actionScene').on('click',actionScene)
 }	
 
-$('button').on('click',setTheScene)
+$('#startScene').on('click',setTheScene)
+
+function actionScene() {
+	$('body').html("")
+	$('body').append("<div class = choose></div>")
+	$('.choose').show()
+	$('body').css('background-image','url(' + events[0].backGroundPic + ')');
+	$('.choose').append('<div class =  event id = ' + 0 + '> what would you do ? <br></div>')
+	$('.choose').append('<div><button class = option >' + events[0].lawfulChoice + '</button>  <button class = option >' + events[0].neutralChoice + '</button>  <button class = option >' + events[0].chaoticChoice + '</button> </div>')
+	$('.option').on('click',afterScript)
+} 
+
+function afterScript() {
+	$('body').html("")
+	$('body').append("<div class = prompt></div>")
+	$('.prompt').show()
+	//$('body').css({'background-image':'url(' + events[0].backGroundPic + ')','background-size':'100% 100%','background-repeat':'no-repeat','height' : '800px'})
+	// $('body').css('background-image','url(https://i.pinimg.com/originals/49/df/93/49df93c9996e7c80736724c2cea3b897.jpg)')
+	$('body').css('background-image','url(' + events[0].backGroundPic + ')');
+	$('.prompt').append('<div class =  event id = ' + 0 + '>'+ events[0].afScript + '</div>')
+	$('.prompt').append('<button class = advance id = actionScene> next>> </button>')
+}
